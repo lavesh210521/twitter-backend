683c41409aed8b867eea12e7b00abc406a503233
LAVESH RAMRAKHIANI <laveshramrakhiani@gmail.com>
2023-09-18 11:41:10 +0530

    replaced constants with env variables

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ import { signin,signout,signup } from "./controllers/AuthController.js";
 import { User } from "./models/Index.js";
 import { auth } from "./middlewares/auth.js";
 import cookieParser from "cookie-parser";
+import dotenv from "dotenv"
+dotenv.config();
 
 //Before Middlewares
 app.use(express.json());
@@ -11,7 +13,6 @@ app.use(express.urlencoded({
     extended: false
 }));
 app.use(cookieParser());
-
 
 app.post("/signup",signup);
 app.post("/signin",signin);
@@ -21,8 +22,8 @@ app.get("/",(req,res) => {
     res.send("Welcome to " + req.url + " page");
 });
 
-app.use("/Protected",auth)
+app.use("/Protected",auth);
 app.get("/Protected", (req,res) => {
     res.send("If you are here that means u must be an Authentic User");
 })
-app.listen(3000);
+app.listen(process.env.PORT || 3000);
