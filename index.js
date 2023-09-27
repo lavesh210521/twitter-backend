20a091240de66ba6c319fdaf2cf73bf7631ac3a4
LAVESH RAMRAKHIANI <laveshramrakhiani@gmail.com>
2023-09-27 17:50:54 +0530

    refactored routes

--- index.js
+++ index.js
@@ -2,9 +2,18 @@ import express from "express";
 const app = express();
 import { signin,signout,signup } from "./controllers/AuthController.js";
 import { User } from "./models/Index.js";
+import { validate } from "./middlewares/validatior/helper.js";
 import { auth } from "./middlewares/auth.js";
 import cookieParser from "cookie-parser";
 import dotenv from "dotenv"
+import { findOne, index } from "./controllers/TweetsController.js";
+
+
+import { userRouter } from "./routes/userRoutes.js";
+import { authRouter } from "./routes/authRoutes.js";
+import { tweetRouter } from "./routes/tweetRoutes.js";
+import { body } from "express-validator";
+import { userTweetRouter } from "./routes/userTweetRoutes.js";
 dotenv.config();
 
 //Before Middlewares
@@ -14,16 +23,17 @@ app.use(express.urlencoded({
 }));
 app.use(cookieParser());
 
-app.post("/signup",signup);
-app.post("/signin",signin);
-app.post("/signout",signout);
-
-app.get("/",(req,res) => {
-    res.send("Welcome to " + req.url + " page");
-});
-
 app.use("/Protected",auth);
 app.get("/Protected", (req,res) => {
     res.send("If you are here that means u must be an Authentic User");
 })
+// app.get("/tweets",auth,index);
+// app.get("/tweets/:tweetId",auth,findOne);
+app.use("/tweets",tweetRouter);
+app.use("/auth",authRouter);
+app.use("/users",userRouter);
+app.use((req,res) => {
+    res.status(400).json({error: "Invalid URL"});
+})
+// app.post("/signin",validate(SigninInputs),signin);
 app.listen(process.env.PORT || 3000);
