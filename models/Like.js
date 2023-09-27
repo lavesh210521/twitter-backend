55930ab49491ccb3c2b8882c3f1c10b9962704a8
LAVESH RAMRAKHIANI <laveshramrakhiani@gmail.com>
2023-09-27 17:31:08 +0530

    added index on Like Model

--- models/Like.js
+++ models/Like.js
@@ -1,21 +1,27 @@
-import {Model,DataTypes} from "sequelize";
-import {sequelize} from "../db/connection.js";
+import { Model, DataTypes } from "sequelize";
+import { sequelize } from "../db/connection.js";
 
-export class Like extends Model{}
+export class Like extends Model { }
 Like.init({
-    id:{
+    id: {
         type: DataTypes.INTEGER,
         primaryKey: true,
         autoIncrement: true
     },
-    user_id:{
-        type:DataTypes.INTEGER
+    user_id: {
+        type: DataTypes.INTEGER
     },
-    tweet_id:{
-        type:DataTypes.INTEGER,
+    tweet_id: {
+        type: DataTypes.INTEGER,
     },
-},{
+}, {
     sequelize,
-    modelName: 'Like'
+    modelName: 'Like',
+    indexes: [
+        {
+            unique: true,
+            fields: ['user_id', 'tweet_id']
+        }
+    ]
 });
 
