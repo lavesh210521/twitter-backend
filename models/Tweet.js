19a6bd1db4d9291619b40ed16857d0accde439ea
LAVESH RAMRAKHIANI <laveshramrakhiani@gmail.com>
2023-09-27 17:33:37 +0530

    changed attribute name as comment_id and added indexes for Tweet

--- models/Tweet.js
+++ models/Tweet.js
@@ -13,11 +13,18 @@ Tweet.init({
     tweet:{
         type:DataTypes.STRING,
     },
+    comment_id:{
+        type:DataTypes.INTEGER
+    },
     image_url:{
         type:DataTypes.STRING
     }
 },{
     sequelize,
-    modelName: 'Tweet'
-});
+    modelName: 'Tweet',
+    indexes:[
+        {fields:['user_id']}
+    ]
+},);
+
 
