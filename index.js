8f5ccbd68da18db9a6c94a03b47de376ce206dfb
LAVESH RAMRAKHIANI <laveshramrakhiani@gmail.com>
2023-10-03 10:29:03 +0530

    updated few options of CORS

--- index.js
+++ index.js
@@ -20,7 +20,20 @@ dotenv.config();
 
 
 //Before Middlewares
-app.use(cors({origin: "http://localhost:4200"}));
+const options = {
+    "origin": 'http://localhost:4200', // or use an array of allowed origins
+    "methods": 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    "credentials": true, 
+    "optionsSuccessStatus": 204
+}
+app.use(cors(options));
+// app.use(function(req, res, next) {
+//     res.header('Access-Control-Allow-Credentials', true);
+//     res.header('Access-Control-Allow-Origin', req.headers.origin);
+//     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,UPDATE,OPTIONS');
+//     res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
+//     next();
+//   });
 app.use(express.json());
 app.use(express.urlencoded({
     extended: false
