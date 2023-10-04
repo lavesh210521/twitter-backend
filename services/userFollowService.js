a51ade981b41e2f321431f7a908d84c473f8e040
LAVESH RAMRAKHIANI <laveshramrakhiani@gmail.com>
2023-10-04 13:12:37 +0530

    added getAuthUserFollowing,followers and removeFollower method

--- services/userFollowService.js
+++ services/userFollowService.js
@@ -1,3 +1,4 @@
+import { Op } from "sequelize";
 import { User, Follow, Like, Tweet } from "../models/Index.js";
 
 export const follow = async (req, res) => {
@@ -32,6 +33,70 @@ export const unfollow = async (req, res) => {
     }
 }
 
+export const getAuthUserFollowings = async (req,res) => {
+    try {
+        const temp = await User.findOne({
+            where: {
+                id: req.userId
+            },
+            include: [
+                {
+                    model: Follow,
+                    as: "Following",
+                    include: [
+                        {
+                            model: User,
+                            as: "Follower",
+                            attributes:["id","first_name","last_name","email","username"]
+                        },
+                    ],
+                },
+            ],
+        })
+        if(temp == undefined){
+            res.status(200).json({ users: {} })
+        }
+        const users = temp.Following.map((value) => {
+            return value.Follower
+        });
+        res.status(200).json({ users: users })
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error: "There is some error while getting Following user!" });
+    }
+}
+export const getAuthUserFollowers = async (req,res) => {
+    try {
+        const temp = await User.findOne({
+            where: {
+                id:req.userId
+            },
+            include: [
+                {
+                    model: Follow,
+                    as: "Follower",
+                    include: [
+                        {
+                            model: User,
+                            as: "Following",
+                            attributes:["id","first_name","last_name","email","username"]
+                        },
+                    ],
+                },
+            ],
+        });
+        if(temp == undefined){
+            res.status(200).json({ users: {} })
+        }
+        const users = temp.Follower.map((value) => {
+            return value.Following
+        });
+        res.status(200).json({ users: users })
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error: "There is some error while getting Follower user!" });
+    }
+}
 export const getUserFollowings = async (req, res) => {
     try {
         console.log(req.params.userId);
@@ -65,10 +130,28 @@ export const getUserFollowings = async (req, res) => {
         res.status(500).json({ error: "There is some error while getting Following user!" });
     }
 }
-
-export const getUserFollowers = async (req, res) => {
+export const removeFollower = async (req,res) => {
     try {
-        const temp = await User.findByPk(54, {
+        const follow = await Follow.destroy({
+            where: {
+                from_user_id: req.body.userId,
+                to_user_id: req.userId
+            }
+        });
+        res.status(200).json({ message: "Unfollow Successful" });
+
+    } catch (error) {
+        res.status(500).json({
+            message: "There is some error while unfollowing user"
+        });
+    }
+}
+export const getUserFollowers = async (req, res) => {
+    try {   
+        const temp = await User.findOne({
+            where: {
+                id:req.params.userId
+            },
             include: [
                 {
                     model: Follow,
