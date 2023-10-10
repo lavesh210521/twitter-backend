545bd9688792df362a122399b8ff60baa2665214
LAVESH RAMRAKHIANI <laveshramrakhiani@gmail.com>
2023-10-10 15:21:43 +0530

    added getAllTweetsWithLikeAndFollow

--- services/tweetService.js
+++ services/tweetService.js
@@ -1,19 +1,29 @@
-import { Tweet, User } from "../models/Index.js";
+import { Model, Op } from "sequelize";
+import { Like, Tweet, User } from "../models/Index.js";
 
 export const getAllTweets = async (req, res) => {
     try {
         let userId = req.userId;
         const tweets = await Tweet.findAll({
             where: {
-                user_id: userId
-            }
+                [Op.and]:[
+                    {user_id: userId},
+                    {comment_id: null}
+                ]
+            },include:[
+                {model: User},
+                {model: Tweet,as:"Comment",include:[
+                    {model: User}
+                ]},
+                {model:Like}
+            ]
         });
         res.status(200).json({
             tweets: tweets
         });
         
     } catch (error) {
-        res.status(500).json({message: "There is some error while fetching tweets"});
+        res.status(500).json({error: "There is some error while fetching tweets"});
     }
     
 }
@@ -47,12 +57,33 @@ export const createTweet = async (req, res) => {
         res.status(200).json({ tweet: tweet });
     } catch (error) {
         console.log(error);
-        res.status(404).json({ message: "There is some error while posting tweet" });
+        res.status(404).json({ error: "There is some error while posting tweet" });
+    }
+}
+export const getAllTweetFromUserWithLikeCountAndCommentCount = async(req,res) => {
+    try {
+        let tweets = await Tweet.findAll({
+            where: {
+                [Op.and]:[
+                {user_id: req.params.userId},
+                {comment_id: null},]
+            },
+            include:[
+                {model: User},
+                {model: Tweet,as:"Comment",include:[
+                    {model: User}
+                ]},
+                {model:Like}
+            ]
+        });
+        res.status(200).json({tweets:tweets});
+    } catch (error) {
+        console.log(error);
+        console.log("error from getAllTweetFromUserWithLikeCountAndCommentCount");
+        res.status(500).json({error:"There is some error while fetching tweets"});
     }
 
-
 }
-
 export const deleteTweet = async (req, res) => {
     try {
         await Tweet.destroy({
@@ -63,7 +94,6 @@ export const deleteTweet = async (req, res) => {
         res.status(200).json({ message: "Tweet Deleted Successfully" });
     } catch (error) {
         console.log(error);
-        res.status(500).json({ message: "There is some error while deleteing tweet!" });
+        res.status(500).json({ error: "There is some error while deleteing tweet!" });
     }
-
 }
