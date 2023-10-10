beb44aa322542b2eb58f75119edbece106fad30d
LAVESH RAMRAKHIANI <laveshramrakhiani@gmail.com>
2023-10-10 15:25:51 +0530

    implemented core logic of getAllTweetFromFollowing

--- services/userFollowTweetService.js
+++ services/userFollowTweetService.js
@@ -1,11 +1,13 @@
-import { Follow, Tweet, User } from "../models/Index.js";
+import { Op } from "sequelize";
+import { Follow, Like, Tweet, User } from "../models/Index.js";
+import { getAuthUserFollowings, getUserFollowings } from "./userFollowService.js";
 
-export const getAllTweetsFromFollowings = async(req,res) => {
+export const getAllTweetsFromFollowings = async (req, res) => {
     try {
-        const follow = await Follow.findByPk(1, {
-            include: "Following"
-        });    
-        const user = await User.findOne({
+        const followings = await User.findOne({
+            where: {
+                id: req.userId
+            },
             include: [
                 {
                     model: Follow,
@@ -14,25 +16,90 @@ export const getAllTweetsFromFollowings = async(req,res) => {
                         {
                             model: User,
                             as: "Follower",
-                            include: [{
-                                model: Tweet,
-                                attributes:["tweet","image_url","createdAt","comment_id"]
-                            }
-                            ]
+                            attributes: ["id", "first_name", "last_name", "email", "username"]
                         },
                     ],
                 },
             ],
+        })
+        if (followings == undefined) {
+            res.status(200).json({ users: {} })
+        }
+        const users = followings.Following.map((value) => {
+            return value.Follower
+        });
+        let ids = [];
+        users.filter((user) => {
+            ids.push(user.id);
+        });
+        let tweets = await Tweet.findAll({
+            limit:2,
+            offset:2,
             where: {
-                id: req.userId
-            }
+                [Op.and]:[
+                {user_id: ids},
+                {comment_id: null},]
+            },
+            include:[
+                {model: User},
+                {model: Tweet,as:"Comment",include:[
+                    {model: User}
+                ]},
+                {model:Like}
+            ]
         });
-        const tweets = user.Following.map((value) => {
-             return (value.Follower.Tweets.map((tweet) => {
-                return tweet;
-            }));
-        });
-        // const tweets =
+
+        // let allTweets = await User.findOne({
+        //     include: [
+        //         {
+        //             model: Follow,
+        //             as: "Following",
+        //             include: [
+        //                 {
+        //                     model: User,
+        //                     as: "Follower",
+        //                     include: [{
+        //                         model: Tweet,
+        //                         attributes: ["tweet", "image_url", "createdAt", "comment_id"],
+        //                         include: [
+        //                             {
+        //                                 model: Like,
+        //                                 attributes: ['id']
+        //                             },
+        //                             {
+        //                                 model: Tweet,
+        //                                 as: "Comment",
+        //                                 attributes: ['id']
+        //                             }
+        //                         ],
+        //                         where: {
+        //                             comment_id: null
+        //                         }
+        //                     }
+        //                     ]
+        //                 },
+        //             ],
+        //         },
+        //     ],
+        //     where: {
+        //         id: req.userId
+        //     }
+        // });
+        // console.log(allTweets);
+        // let tweets = allTweets.Following.map((value) => {
+        //     if(value && value.Follower){
+        //         return (value.Follower.Tweets.map((tweet) => {
+        //             return tweet;
+        //         }));
+        //     }
+        // });
+        // tweets = allTweets.flat();
+        // tweets = tweets.filter((tweet) => {
+        //     if(tweet != null){
+        //         return tweet;
+        //     }
+        // })
+        // console.log(tweets);
         res.status(200).json({ tweets: tweets });
     } catch (error) {
         console.log(error);
