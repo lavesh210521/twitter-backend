ce87a8afcfd8d23d7fd4675946320ec83c428361
LAVESH RAMRAKHIANI <laveshramrakhiani@gmail.com>
2023-10-11 15:38:11 +0530

    fetched credentials from env and removed port

--- db/connection.js
+++ db/connection.js
@@ -1,8 +1,9 @@
 import { Sequelize } from "sequelize";
 import dotenv from "dotenv"
 dotenv.config();
-const sequelize = new Sequelize('twitter', process.env.DB_USERNAME, process.env.DB_PASSWORD, {
-    host: 'localhost',
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+    host: process.env.HOST,
     dialect: 'mysql'
 });
 
