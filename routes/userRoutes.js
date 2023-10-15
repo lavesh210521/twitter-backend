9f3cb02c76cf4b742b3aa822d9228d25238df12f
LAVESH RAMRAKHIANI <laveshramrakhiani@gmail.com>
2023-10-15 19:11:52 +0530

    removed params from url

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,34 +1,32 @@
 import express from "express";
-import { getAnyUserProfile, getUser, getUsersBySearch, updateUser, validateUser } from "../services/userService.js";
 import { auth } from "../middlewares/auth.js";
-import { userProfileViewValidationRules, userSearchValidationRules, userUpdateValidationRules, validateInputs } from "../middlewares/validatior/userValidator.js";
-import { handleValidationErrors } from "../middlewares/validatior/errorHandler.js";
-import { validate } from "../middlewares/validatior/helper.js";
+import { validate } from "../exception_handling/errorHandler.js";
+import { getAnyUserProfile, getUser, getUsersBySearch, updateUser, validateUser } from "../services/userService.js";
+import { userProfileViewValidationRules, userSearchValidationRules, userUpdateValidationRules } from "../middlewares/validatior/userValidator.js";
 import { userTweetValidationRules } from "../middlewares/validatior/userTweetValidator.js";
 import { getAllTweetsFromUser } from "../services/userTweetService.js";
 import { getFollowValidationRules, userFollowValidationRules, userRemoveFollowerValidationRules, userUnfollowValidationRules } from "../middlewares/validatior/userFollowValidator.js";
 import { follow, getAuthUserFollowers, getAuthUserFollowings, getUserFollowers, getUserFollowings, getUserWithFollow, removeFollower, unfollow } from "../services/userFollowService.js";
+import { paginateValidationRules } from "../middlewares/validatior/commonValidatior.js";
+import { getAllFollowingTweets } from "../services/tempService.js";
 
 const userRouter = express.Router();
 
-// User Routes
-userRouter.get("/profile", auth, getUser);
-//using same rule; as it satisfied this request so i'm not creating other validation rule
-userRouter.get("/:userId/profile-with-follow", auth,validate(getFollowValidationRules), getUserWithFollow);
-userRouter.get("/:userId/profile",validate(userProfileViewValidationRules),getAnyUserProfile);
-userRouter.post("/update", auth, validate(userUpdateValidationRules), updateUser)
-userRouter.get("/:userId/tweets", auth, validate(userTweetValidationRules), getAllTweetsFromUser)
-// userRouter.get("/followings/:userId",auth,getUserFollowings);
-// userRouter.get("/followers/:userId",auth,getUserFollowers);
-userRouter.post("/follow", auth, validate(userFollowValidationRules), follow);
-userRouter.post("/unfollow", auth, validate(userUnfollowValidationRules), unfollow);
-userRouter.post("/remove-follower",auth,validate(userRemoveFollowerValidationRules),removeFollower);
-userRouter.get("/search/:searchKeyword",auth,validate(userSearchValidationRules),getUsersBySearch);
-userRouter.get("/followings",auth,getAuthUserFollowings);
-userRouter.get("/followers",auth,getAuthUserFollowers);
-userRouter.get("/:userId/followings",auth,validate(getFollowValidationRules), getUserFollowings);
-//using getFollowingvalidationRules 
-userRouter.get("/:userId/followers",auth,validate(getFollowValidationRules), getUserFollowers);
-userRouter.get("/validate",auth,validateUser);
+userRouter.get("/auth/profile", auth, getUser);
+userRouter.get("/profile-with-follow", auth, validate(getFollowValidationRules), getUserWithFollow);
+userRouter.get("/profile", validate(userProfileViewValidationRules), getAnyUserProfile);
+userRouter.patch("", auth, validate(userUpdateValidationRules), updateUser)
+userRouter.get("/tweets", auth, validate(userTweetValidationRules), getAllTweetsFromUser)
+userRouter.put("/follow", auth, validate(userFollowValidationRules), follow);
+userRouter.put("/unfollow", auth, validate(userUnfollowValidationRules), unfollow);
+userRouter.put("/remove-follower", auth, validate(userRemoveFollowerValidationRules), removeFollower);
+userRouter.get("/search", auth, validate(userSearchValidationRules), getUsersBySearch);
+userRouter.get("/auth/followings", auth, validate(paginateValidationRules), getAuthUserFollowings);
+userRouter.get("/auth/followers", auth, validate(paginateValidationRules), getAuthUserFollowers);
+//add pagination in follower-following of user's profile 
+userRouter.get("/followings", auth, validate(getFollowValidationRules), getUserFollowings);
+userRouter.get("/followers", auth, validate(getFollowValidationRules), getUserFollowers);
+userRouter.get("/validate", auth, validateUser);
+userRouter.get("/all-following-tweets",getAllFollowingTweets);
 
 export { userRouter };    
