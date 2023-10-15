73bbe28f59225c6bd1687568bca3bf3835d63a64
LAVESH RAMRAKHIANI <laveshramrakhiani@gmail.com>
2023-10-15 19:12:40 +0530

    change in import path

--- routes/userTweetRoutes.js
+++ routes/userTweetRoutes.js
@@ -1,12 +1,12 @@
 import express from "express";
 import { auth } from "../middlewares/auth.js";
-import { validate } from "../middlewares/validatior/helper.js";
+import { validate } from "../exception_handling/errorHandler.js";
 import { userTweetValidationRules } from "../middlewares/validatior/userTweetValidator.js";
 import { getAllTweetsFromUser } from "../services/userTweetService.js";
 
 const userTweetRouter = express.Router();
+//i think this route is not getting used will throw this out later!
+userTweetRouter.get("", auth, validate(userTweetValidationRules), getAllTweetsFromUser);
 
-userTweetRouter.get("",auth,validate(userTweetValidationRules),getAllTweetsFromUser);
-
-export {userTweetRouter}
+export { userTweetRouter }
 
