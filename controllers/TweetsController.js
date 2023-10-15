b74c91614f48f0c327ff4ca56fbd9f5825842c6a
LAVESH RAMRAKHIANI <laveshramrakhiani@gmail.com>
2023-10-15 18:57:34 +0530

    deleted unwanted files

--- controllers/TweetsController.js
+++
@@ -1,8 +0,0 @@
-import { getAllTweets,getTweetFromId } from "../services/tweetService.js";
-
-export const index = (req,res) => {
-    return getAllTweets(req,res);
-}
-export const findOne = (req,res) => {
-    return getTweetFromId(req,res);
-}
