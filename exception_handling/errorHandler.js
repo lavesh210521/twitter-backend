5f76895f90b4525478bd90dc996ea8d997ff8fd1
LAVESH RAMRAKHIANI <laveshramrakhiani@gmail.com>
2023-10-15 18:58:32 +0530

    refactored validation errorHandler inside error_handling

---
+++ exception_handling/errorHandler.js
@@ -0,0 +1,16 @@
+import { body, validationResult } from "express-validator";
+export const validate = validations => {
+  return async (req, res, next) => {
+    for (let validation of validations) {
+      const result = await validation.run(req);
+      if (result.errors.length) break;
+      console.log(result);
+    }
+
+    const errors = validationResult(req);
+    if (errors.isEmpty()) {
+      return next();
+    }
+    res.status(422).json({ error: errors.array()[0].msg });
+  };
+};
