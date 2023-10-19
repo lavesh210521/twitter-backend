c6c5c0901cc7165abee57cddb89792546444367a
LAVESH RAMRAKHIANI <laveshramrakhiani@gmail.com>
2023-10-19 12:51:01 +0530

    loaded credentials from env file

--- config/emailHandler.js
+++ config/emailHandler.js
@@ -1,28 +1,33 @@
-import nodemailer from "nodemailer"
-import dotenv from "dotenv"
+import nodemailer from "nodemailer";
+import dotenv from "dotenv";
 dotenv.config();
 
 const transporter = nodemailer.createTransport({
-    host: "sandbox.smtp.mailtrap.io",
-    port: 2525,
-    auth: {
-      user: "ab2dff21eefcea",
-      pass: "eb0e37a89f5a8d"
-    }
+  host: process.env.EMAIL_HOST,
+  port: 2525,
+  auth: {
+    user: process.env.EMAIL_USERNAME,
+    pass: process.env.EMAIL_PASSWORD
+  }
 });
 
-export const sendEmail = async(from,to,subject,text) => {
-  // send mail with defined transport object
-  const info = await transporter.sendMail({
-    from: from, // sender address
-    to: to, // list of receivers
-    subject: subject, // Subject line
-    text: text
-  });
-
-  return ("Message sent: %s", info.messageId);
+export const sendEmail = async (from, to, subject, text) => {
+  console.log("Send Email Called!");
+  try {
+    // send mail with defined transport object
+    const info = await transporter.sendMail({
+      from: from, // sender address
+      to: to, // list of receivers
+      subject: subject, // Subject line
+      text: text
+    });
+    return ("Message sent: %s", info.messageId ); 
+  } catch (error) {
+    console.log("There is some problem while sending email " + error);
+  }
+  
 }
 
-export const reportError = async(subject,text) => {
-  await sendEmail("app@media.net","dev@media.net",subject,text);
+export const reportError = async (subject, text) => {
+  await sendEmail("app@media.net", "dev@media.net", subject, text);
 }
