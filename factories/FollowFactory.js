d73a0879aea86007f7e67050ed859f07da7b3465
LAVESH RAMRAKHIANI <laveshramrakhiani@gmail.com>
2023-10-19 12:52:43 +0530

    updated logic

--- factories/FollowFactory.js
+++ factories/FollowFactory.js
@@ -7,7 +7,7 @@ export async function create(count){
     let users = await User.findAll();
     let result = [];
     for(var i = 0;i < count;++i){
-        let fake_from_id = faker.helpers.arrayElement(users).id;
+        let fake_from_id = users[i].id;
         let fake_to_id = (fake_from_id+1) % (users.length-1);
         if(fake_to_id == 0){
             fake_to_id = 1;
@@ -24,7 +24,7 @@ export async function build(count){
     let users = await User.findAll();
     let result = [];
     for(var i = 0;i < count;++i){
-        let fake_from_id = faker.helpers.arrayElement(users).id;
+        let fake_from_id = users[i].id;
         let fake_to_id = (fake_from_id+1) % (users.length-1);
         if(fake_to_id == 0){
             fake_to_id = 1;
