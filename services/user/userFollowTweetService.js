d89d1b279e5103286ba281c439f6e6d69f8feca1
LAVESH RAMRAKHIANI <laveshramrakhiani@gmail.com>
2023-10-23 16:22:17 +0530

    removed like's other attributed while fetching tweets

--- services/user/userFollowTweetService.js
+++ services/user/userFollowTweetService.js
@@ -1,63 +1,66 @@
 import { Op } from "sequelize";
 import { Follow, Like, Tweet, User } from "../../models/Index.js";
 
-export const getAllTweetsFromFollowings = async (userId,limit,offset) => {
-    
-        const followings = await User.findOne({
-            where: {
-                id: userId
-            },
-            include: [
-                {
-                    model: Follow,
-                    as: "Following",
-                    include: [
-                        {
-                            model: User,
-                            as: "Follower",
-                            attributes: ["id"]
-                        },
-                    ],
-                },
-            ],
-        })
-        if (followings == undefined) {
-            res.status(200).json({ users: {} })
-        }
-        const users = followings.Following.map((value) => {
-            return value.Follower
-        });
-        let ids = [];
-        users.filter((user) => {
-            ids.push(user.id);
-        });
-        let totalTweetCount = await Tweet.findAndCountAll({
-            where: {
-                [Op.and]: [
-                    { user_id: ids },
-                    { comment_id: null },
-                ]
-            }
-        });
-        let tweets = await Tweet.findAll({
-            limit: Number(limit),
-            offset: Number(offset),
-            where: {
-                [Op.and]: [
-                    { user_id: ids },
-                    { comment_id: null },]
-            },
-            include: [
-                { model: User },
-                {
-                    model: Tweet, as: "Comment", 
-                    include: [
-                        { model: User }
-                    ]
-                },
-                { model: Like }
-            ]
-        });
+export const getAllTweetsFromFollowings = async (userId, limit, offset) => {
 
-        return [tweets, totalTweetCount.count];
+    const followings = await User.findOne({
+        where: {
+            id: userId
+        },
+        include: [
+            {
+                model: Follow,
+                as: "Following",
+                include: [
+                    {
+                        model: User,
+                        as: "Follower",
+                        attributes: ["id"]
+                    },
+                ],
+            },
+        ],
+    })
+    if (followings == undefined) {
+        res.status(200).json({ users: {} })
+    }
+    const users = followings.Following.map((value) => {
+        return value.Follower
+    });
+    let ids = [];
+    users.filter((user) => {
+        ids.push(user.id);
+    });
+    let totalTweetCount = await Tweet.findAndCountAll({
+        where: {
+            [Op.and]: [
+                { user_id: ids },
+                { comment_id: null },
+            ]
+        }
+    });
+    let tweets = await Tweet.findAll({
+        limit: Number(limit),
+        offset: Number(offset),
+        where: {
+            [Op.and]: [
+                { user_id: ids },
+                { comment_id: null },]
+        },
+        include: [
+            { model: User },
+            {
+                model: Tweet, as: "Comment",
+                include: [
+                    { model: User }
+                ]
+            },
+            {
+                model: Like,
+                attributes: ['id']
+            }
+        ]
+    });
+
+    return [tweets, totalTweetCount.count];
 }
