381d48cb7e6066340a99343d9260c1cd13336366
LAVESH RAMRAKHIANI <laveshramrakhiani@gmail.com>
2023-11-21 14:19:37 +0530

    hardcoded credentials from email

--- config/emailHandler.js
+++ config/emailHandler.js
@@ -3,11 +3,11 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const transporter = nodemailer.createTransport({
-  host: process.env.EMAIL_HOST,
+  host: "sandbox.smtp.mailtrap.io",
   port: 2525,
   auth: {
-    user: process.env.EMAIL_USERNAME,
-    pass: process.env.EMAIL_PASSWORD
+    user: "ab2dff21eefcea",
+    pass: "eb0e37a89f5a8d"
   }
 });
 
